3083ed1f4eabedfa1a34a283e1b64344573c48dc
Harshil Gupta <harshilgupta1808@gmail.com>
2025-08-19 18:28:46 +0530

    adding one more tag
    Signed-off-by: Harshil Gupta <harshilgupta1808@gmail.com>

--- scripts/makefiles/Docker.mk
+++ scripts/makefiles/Docker.mk
@@ -37,7 +37,7 @@ create-fake-debugimg: prepare-docker-buildx
 prepare-docker-buildx:
 	@echo "::group:: prepare-docker-buildx"
 	docker buildx inspect jaeger-build > /dev/null || docker buildx create --use --name=jaeger-build --buildkitd-flags="--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host" --driver-opt="network=host"
-	docker inspect registry > /dev/null || docker run --rm -d -p 5000:5000 --name registry registry:2
+	docker inspect registry > /dev/null || docker run --rm -d -p $DOCKER_REGISTRY_PORT:5000 --name registry registry:2
 	@echo "::endgroup::"
 
 .PHONY: clean-docker-buildx
